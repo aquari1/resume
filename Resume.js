8b567ee0f707c8e8892dead547012f773aa09fda
daydreamlantern <132487731+aquari1@users.noreply.github.com>
2024-02-17 18:30:32 +0800

    Create Resume.js

---
+++ Resume.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+
+
+function Resume() {
+    return (
+        
+         <body>
+          <title>My Resume</title>
+            <style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
+</style>
+            
+    <div class="card">
+    <div class="header">
+    <div class="profile-container">
+        
+  <img src="./images/face.jpg" alt="Zac Lauron" class="profile-image"></img>
+  
+  <div class='header1'>
+  <h1>Zac Lauron P. Palconit</h1>
+  <h2>Web Developer</h2>
+  <p>Email: zaclauron.palconit@gmail.com</p>
+  <p>Mobile num: +63 929 723 4181</p>
+
+  
+  </div>
+
+  </div>
+  </div>
+
+  <div class="section">
+    <h2>Summary</h2>
+    <p>Equipped with a record of success in consistently identifying and providing the technological
+     needs of companies through ingenious innovation, Proficient in developing websites. </p>
+  </div>
+  
+  <div class="section">
+    <h2>Education</h2>
+    <h3>Holy Cross of Davao College</h3>
+    <p>Bachelor of Science in Information Technology</p>
+    <p>Graduated in 2019</p>
+  </div>
+  
+  <div class="section">
+    <h2>Experience</h2>
+    <h3>Web Developer, Alorica Philippines</h3>
+    <p>July 2021 - 2023</p>
+    <ul>
+      <li>Developed and maintained company website using HTML, CSS, and JavaScript.</li>
+      <li>Collaborated with cross-functional teams to implement new features and improve website performance.</li>
+      <li>Optimized website for search engines to increase organic traffic.</li>
+    </ul>
+  </div>
+  
+  <div class="section">
+    <h2>Skills</h2>
+    <ul>
+      <li><span class="highlight">HTML</span>, <span class="highlight">CSS</span>, <span class="highlight">JavaScript</span></li>
+      <li>Responsive Web Design</li>
+      <li>React</li>
+    </ul>
+  </div> 
+  </div>
+ </body>
+
+
+    );
+  }
+  
+  export default Resume;
